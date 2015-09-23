e0e0fb4d0760f26576c8a8985e1353a3c1674932
aarononi <aarononi@optusnet.com.au>
2015-09-23 12:01:50 +1000

    creates the database in the current directory

---
+++ workspace/cs4920_project/createdb.pl
@@ -0,0 +1,144 @@
+#!/usr/bin/perl
+
+use DBI;
+use strict;
+
+my $driver   = "SQLite"; 
+my $database = "project.db";
+my $dsn = "DBI:$driver:dbname=$database";
+my $userid = "";
+my $password = "";
+my $dbh = DBI->connect($dsn, $userid, $password, { RaiseError => 1 }) 
+                      or die $DBI::errstr;
+
+print "Opened database successfully\n";
+
+my $stmt = qq(CREATE TABLE user (
+	id INTEGER PRIMARY KEY,
+	fName TEXT,
+	lName TEXT,
+	username TEXT,
+	password TEXT,
+	email TEXT,
+	height INTEGER,
+	weight INTEGER,
+	dob TEXT,
+	goal TEXT
+););
+my $rv = $dbh->do($stmt);
+if($rv < 0){
+   /*print $DBI::errstr;*/
+} else {
+   print "Table created successfully\n";
+}
+
+my $stmt = qq(CREATE TABLE weight (
+	value INTEGER,
+	userid INTEGER,
+	year INTEGER,
+	month INTEGER,
+	day INTEGER,
+	time INTEGER,
+	FOREIGN KEY(userid) REFERENCES user(id)
+););
+my $rv = $dbh->do($stmt);
+if($rv < 0){
+   print $DBI::errstr;
+} else {
+   print "Table created successfully\n";
+}
+
+my $stmt = qq(create TABLE meal (
+	id INTEGER PRIMARY KEY,
+	userid INTEGER,
+	name TEXT,
+	date TEXT,
+	time INTEGER,
+	FOREIGN KEY(userid) REFERENCES user(id)
+););
+my $rv = $dbh->do($stmt);
+if($rv < 0){
+   print $DBI::errstr;
+} else {
+   print "Table created successfully\n";
+}
+
+my $stmt = qq(create TABLE food (
+	id INTEGER PRIMARY KEY,
+	name TEXT,
+	energy TEXT,
+	protein TEXT,
+	fat TEXT,
+	monounsatfat TEXT,
+	polyunsatfat TEXT,
+	satfat TEXT,
+	transfat TEXT,
+	carbs TEXT,
+	fibre TEXT,
+	sugars TEXT,
+	cholesterol TEXT
+););
+my $rv = $dbh->do($stmt);
+if($rv < 0){
+   print $DBI::errstr;
+} else {
+   print "Table created successfully\n";
+}
+
+my $stmt = qq(create TABLE minerals (
+	fid INTEGER,
+	calcium TEXT,
+	iron TEXT,
+	magnesium TEXT,
+	manganese TEXT,
+	phosphorous TEXT,
+	potassium TEXT,
+	sodium TEXT,
+	zinc TEXT,
+	FOREIGN KEY(fid) REFERENCES food(id)
+););
+my $rv = $dbh->do($stmt);
+if($rv < 0){
+   print $DBI::errstr;
+} else {
+   print "Table created successfully\n";
+}
+
+my $stmt = qq(create TABLE vitamins (
+	fid INTEGER,
+	A TEXT,
+	B1 TEXT,
+	B2 TEXT,
+	B3 TEXT,
+	B5 TEXT,
+	B6 TEXT,
+	B7 TEXT,
+	B9 TEXT,
+	B12 TEXT,
+	C TEXT,
+	D TEXT,
+	E TEXT,
+	K TEXT,
+	FOREIGN KEY(fid) REFERENCES food(id)
+););
+my $rv = $dbh->do($stmt);
+if($rv < 0){
+   print $DBI::errstr;
+} else {
+   print "Table created successfully\n";
+}
+
+my $stmt = qq(create TABLE meal_contains (
+	mid INTEGER,
+	fid INTEGER,
+	FOREIGN KEY(mid) REFERENCES meal(id),
+	FOREIGN KEY(fid) REFERENCES food(id)
+););
+my $rv = $dbh->do($stmt);
+if($rv < 0){
+   print $DBI::errstr;
+} else {
+   print "Table created successfully\n";
+}
+
+$dbh->disconnect();
