8407f05aa5d1dcce990dcd7d44248fe5f8fa582e
srav300 <shyam9691@yahoo.in>
2015-09-27 15:25:55 +1000

    Update createdb.pl
    Updated User table and added Calories Table

--- workspace/cs4920_project/createdb.pl
+++ workspace/cs4920_project/createdb.pl
@@ -6,9 +6,9 @@ use strict;
 my $driver   = "SQLite"; 
 my $database = "project.db";
 my $dsn = "DBI:$driver:dbname=$database";
-my $userid = "";
+my $uid = "";
 my $password = "";
-my $dbh = DBI->connect($dsn, $userid, $password, { RaiseError => 1 }) 
+my $dbh = DBI->connect($dsn, $uid, $password, { RaiseError => 1 }) 
                       or die $DBI::errstr;
 
 print "Opened database successfully\n";
@@ -22,7 +22,8 @@ my $stmt = qq(CREATE TABLE user (
 	email TEXT,
 	height INTEGER,
 	weight INTEGER,
-	dob TEXT,
+	age INTEGER,
+	exercise TEXT,
 	goal TEXT
 ););
 my $rv = $dbh->do($stmt);
@@ -34,12 +35,12 @@ if($rv < 0){
 
 my $stmt = qq(CREATE TABLE weight (
 	value INTEGER,
-	userid INTEGER,
+	uid INTEGER,
 	year INTEGER,
 	month INTEGER,
 	day INTEGER,
 	time INTEGER,
-	FOREIGN KEY(userid) REFERENCES user(id)
+	FOREIGN KEY(uid) REFERENCES user(id)
 ););
 my $rv = $dbh->do($stmt);
 if($rv < 0){
@@ -50,11 +51,11 @@ if($rv < 0){
 
 my $stmt = qq(create TABLE meal (
 	id INTEGER PRIMARY KEY,
-	userid INTEGER,
+	uid INTEGER,
 	name TEXT,
 	date TEXT,
 	time INTEGER,
-	FOREIGN KEY(userid) REFERENCES user(id)
+	FOREIGN KEY(uid) REFERENCES user(id)
 ););
 my $rv = $dbh->do($stmt);
 if($rv < 0){
@@ -137,6 +138,19 @@ my $stmt = qq(create TABLE meal_contains (
 my $rv = $dbh->do($stmt);
 if($rv < 0){
    print $DBI::errstr;
+} else {
+   print "Table created successfully\n";
+}
+
+my $stmt = qq(CREATE TABLE calories (
+	uid INTEGER,
+	goal INTEGER,
+	date TEXT,
+	FOREIGN KEY(uid) REFERENCES user(id)
+););
+my $rv = $dbh->do($stmt);
+if($rv < 0){
+   /*print $DBI::errstr;*/
 } else {
    print "Table created successfully\n";
 }
